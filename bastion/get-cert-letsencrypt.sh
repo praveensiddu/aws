5c786bf0c5a4d751834407eb68de723772367788
Praveen SIDDU <praveensiddu@users.noreply.github.com>
2020-11-14 22:32:43 -0500

    domain

--- bastion/get-cert-letsencrypt.sh
+++ bastion/get-cert-letsencrypt.sh
@@ -1,3 +1,9 @@
+if [[ $1 == "" ]]
+then
+        echo "Usage $0 <example.com>"
+        exit 1
+fi
+
 
 sudo amazon-linux-extras install epel
 sudo yum install certbot -y
@@ -5,4 +11,5 @@ sudo yum install certbot -y
 # Temporariry stop haproxy
 sudo systemctl stop haproxy
 
-sudo certbot certonly --standalone --preferred-challenges http --http-01-port 80 -d example.com -d www.example.com
+sudo certbot certonly --standalone --preferred-challenges http --http-01-port 80 -d $1 -d www.$1
+

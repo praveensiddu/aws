6edd5b870410baefa5cd7fa1ba4f7dfc1eaf0933
Praveen SIDDU <praveensiddu@users.noreply.github.com>
2020-11-14 23:24:40 -0500

    certs

--- bastion/get-cert-letsencrypt.sh
+++ bastion/get-cert-letsencrypt.sh
@@ -13,3 +13,12 @@ sudo systemctl stop haproxy
 
 sudo certbot certonly --standalone --preferred-challenges http --http-01-port 80 -d $1 -d www.$1
 
+sudo ls /etc/letsencrypt/live/$1
+sudo mkdir -p /etc/haproxy/certs
+
+# create the combined file 
+DOMAIN='$1" sudo -E bash -c 'cat /etc/letsencrypt/live/$DOMAIN/fullchain.pem /etc/letsencrypt/live/$DOMAIN/privkey.pem > /etc/haproxy/certs/$DOMAIN.pem'
+
+
+sudo chmod -R go-rwx /etc/haproxy/certs
+

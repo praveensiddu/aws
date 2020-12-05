8df0f10f4f818834a38b472310bbf8ce40816bf6
Praveen SIDDU <praveensiddu@users.noreply.github.com>
2020-12-04 21:58:18 -0500

    Update cloud-init.sh

--- bastion/cloud-init.sh
+++ bastion/cloud-init.sh
@@ -7,13 +7,13 @@ curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip
 unzip awscliv2.zip
 sudo ./aws/install
 
+
 sudo systemctl enable docker
 sudo systemctl start docker
-cd /etc/haproxy
-sudo cp haproxy.cfg haproxy.cfg.orig
-sudo rm haproxy.cfg
-sudo wget https://raw.githubusercontent.com/praveensiddu/aws/main/bastion/haproxy.cfg
-sudo yum install haproxy -y
+
+sudo wget https://raw.githubusercontent.com/praveensiddu/aws/main/bastion/install_haproxy.sh -O install_haproxy.sh
+bash install_haproxy.sh
+
 echo "set -o vi" >> ~/.bashrc
 sudo bash -c 'echo set -o vi >> ~root/.bashrc'
 

f50d1a941d70a1e05a0d4d01be848fe6245583df
Praveen SIDDU <praveensiddu@users.noreply.github.com>
2020-12-04 21:14:40 -0500

    Update create_and_assign_secgrp.sh

--- bastion/create_and_assign_secgrp.sh
+++ bastion/create_and_assign_secgrp.sh
@@ -1,8 +1,13 @@
+if [[ $1 == "" ]]
+then
+        echo "Usage $0 access-via-bastion-secgrp"
+        exit 1
+fi
 myinstanceid=$(wget -q -O - http://169.254.169.254/latest/meta-data/instance-id)
 echo $myinstanceid
 current_security_groups=$(aws ec2 describe-instances --instance-ids $myinstanceid --query Reservations[].Instances[].SecurityGroups[*].GroupId --output text)
 echo $current_security_groups
-aws ec2 create-security-group --description "Bastion security group to access to other hosts" --group-name access-via-bastion-secgrp --tag-specifications "ResourceType=security-group,Tags=[{Key=Name,Value=access-via-bastion}]" --query GroupId --output text
-newsecuritygroup=$(aws ec2 describe-security-groups --group-names access-via-bastion-secgrp --query SecurityGroups[*].GroupId --output text)
-echo $newsecuritygroup
-aws ec2 modify-instance-attribute --instance-id $myinstanceid --groups $current_security_groups $newsecuritygroup
+aws ec2 create-security-group --description "Bastion security group to access to other hosts" --group-name $1 --tag-specifications "ResourceType=security-group,Tags=[{Key=Name,Value=access-via-bastion}]" --query GroupId --output text
+newsecuritygroupId=$(aws ec2 describe-security-groups --group-names $1 --query SecurityGroups[*].GroupId --output text)
+echo $newsecuritygroupId
+aws ec2 modify-instance-attribute --instance-id $myinstanceid --groups $current_security_groups $newsecuritygroupId

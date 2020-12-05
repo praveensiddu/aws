8f938d55f88bea869e60c8982d730c058ffab65f
Praveen SIDDU <praveensiddu@users.noreply.github.com>
2020-12-05 01:51:00 -0500

    Update create-instance.sh

--- lamp/create-instance.sh
+++ lamp/create-instance.sh
@@ -5,6 +5,6 @@ then
         exit 1
 fi
 export AMZLINUZ2_AMI=$(aws ssm get-parameters --names /aws/service/ami-amazon-linux-latest/amzn2-ami-hvm-x86_64-gp2 --query 'Parameters[0].[Value]' --output text)
-wget https://raw.githubusercontent.com/praveensiddu/aws/main/tomcat-on-k3s/cloud-init.sh -O cloud-init.sh
+wget https://raw.githubusercontent.com/praveensiddu/aws/main/lamp/cloud-init.sh -O cloud-init.sh
 
 aws ec2 run-instances --image-id $AMZLINUZ2_AMI --count 1 --instance-type t2.micro --key-name  $MYSSHKEYNAME 

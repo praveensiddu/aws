1a52ce3fc11645800c5585165272fd5778938a3b
Praveen SIDDU <praveensiddu@users.noreply.github.com>
2020-12-04 20:58:11 -0500

    Update wordpress-db.sql

--- wordpress/wordpress-db.sql
+++ wordpress/wordpress-db.sql
@@ -1,5 +1,6 @@
+DROP USER 'wordpress-user'@'localhost';
 CREATE USER 'wordpress-user'@'localhost' IDENTIFIED BY 'your_strong_password';
-CREATE DATABASE `wordpress-db`;
+CREATE DATABASE IF NOT EXISTS `wordpress-db`;
 GRANT ALL PRIVILEGES ON `wordpress-db`.* TO "wordpress-user"@"localhost";
 FLUSH PRIVILEGES;
 exit

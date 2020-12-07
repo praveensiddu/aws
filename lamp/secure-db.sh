524489d9baa6d36fefc5ed8c7dfd7ba336ac9b2e
Praveen SIDDU <praveensiddu@users.noreply.github.com>
2020-12-06 22:27:06 -0500

    Update secure-db.sh

--- lamp/secure-db.sh
+++ lamp/secure-db.sh
@@ -1,16 +1,12 @@
 #! /bin/bash
 sudo systemctl start mariadb
-echo set root password
-echo Type Y to remove the anonymous user accounts.
 
-echo Type Y to disable the remote root login.
+mysqladmin -u root password "$DATABASE_PASS"
+mysql -u root -p"$DATABASE_PASS" -e "UPDATE mysql.user SET Password=PASSWORD('$DATABASE_PASS') WHERE User='root'"
+mysql -u root -p"$DATABASE_PASS" -e "DELETE FROM mysql.user WHERE User='root' AND Host NOT IN ('localhost', '127.0.0.1', '::1')"
+mysql -u root -p"$DATABASE_PASS" -e "DELETE FROM mysql.user WHERE User=''"
+mysql -u root -p"$DATABASE_PASS" -e "DELETE FROM mysql.db WHERE Db='test' OR Db='test\_%'"
+mysql -u root -p"$DATABASE_PASS" -e "FLUSH PRIVILEGES"
 
-echo Type Y to remove the test database.
-
-echo Type Y to reload the privilege tables and save your changes.
-
-read -t 10 -p "Read the instruction above and press enter"
-
-sudo mysql_secure_installation
 sudo systemctl restart mariadb
 sudo systemctl enable mariadb

addd2acc699f736db735dcc721ae6be11d8e6696
Praveen SIDDU <praveensiddu@users.noreply.github.com>
2020-12-08 19:56:01 -0500

    Update cloud-init.sh

--- bastion/cloud-init.sh
+++ bastion/cloud-init.sh
@@ -12,7 +12,7 @@ sudo systemctl enable docker
 sudo systemctl start docker
 
 # install ansible for automation( ideally a dedicated orchestration host is recommended)
-sudo amazon-linux-extras install ansible2
+sudo amazon-linux-extras install -y ansible2
 
 # install haproxy only if you plan to run load balancer on bastion(A dedicated load balancer is recommended)
 sudo wget https://raw.githubusercontent.com/praveensiddu/aws/main/bastion/install_haproxy.sh -O install_haproxy.sh

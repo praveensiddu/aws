da04a500b6834663bff62aba14076e399db8b176
Praveen SIDDU <praveensiddu@users.noreply.github.com>
2020-12-12 00:31:29 -0500

    Update get-private-ip.sh

--- utils/get-private-ip.sh
+++ utils/get-private-ip.sh
@@ -3,4 +3,4 @@ if [[ "$1" == "" ]]; then
         echo "Usage $0 <value of tag Name of instance>"
         exit 1
 fi
-aws ec2 describe-instances --filters "Name=tag:Name,Values=lamp" "Name=instance-state-name,Values=running" --query "Reservations[].Instances[].{Instance:PrivateIpAddress}" --output=text
+aws ec2 describe-instances --filters "Name=tag:Name,Values=$1" "Name=instance-state-name,Values=running" --query "Reservations[].Instances[].{Instance:PrivateIpAddress}" --output=text

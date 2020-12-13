19c68beb0a9457cc5b4349187b42aff8fddd67e1
Praveen SIDDU <praveensiddu@users.noreply.github.com>
2020-12-13 17:08:14 -0500

    Update create-instance.sh

--- k3s-aws/create-instance.sh
+++ k3s-aws/create-instance.sh
@@ -4,7 +4,15 @@ then
         echo "https://console.aws.amazon.com/ec2/v2/home?region=us-east-1#KeyPairs:"
         exit 1
 fi
-export UBUNTU_AMI=$(aws ec2 describe-images  --owners 099720109477  --filters Name=name,Values=ubuntu/images/hvm-ssd/ubuntu-xenial-16.04-amd64-server-*  --query 'sort_by(Images,&CreationDate)[-1].ImageId' --output text)
-rm -f cloud-init.sh && wget https://raw.githubusercontent.com/praveensiddu/aws/main/tomcat-on-k3s/cloud-init.sh
+if [[ $INSTNAME == "" ]]
+then
+        echo "Set env INSTNAME to the name of the instance. Name must be unique"
+        exit 1
+fi
 
-aws ec2 run-instances --image-id $UBUNTU_AMI --count 1 --instance-type t2.micro --key-name  $MYSSHKEYNAME --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=k3s-aws}]"
+export UBUNTU_AMI=$(aws ec2 describe-images  --owners 099720109477  --filters Name=name,Values=ubuntu/images/hvm-ssd/ubuntu-xenial-16.04-amd64-server-*  --query 'sort_by(Images,&CreationDate)[-1].ImageId' --output text)
+wget https://raw.githubusercontent.com/praveensiddu/aws/main/k3s-aws/cloud-init.sh -O cloud-init.sh
+wget  https://raw.githubusercontent.com/praveensiddu/aws/main/utils/create-secgrp.sh -O create-secgrp.sh 
+bash create-secgrp.sh $INSTNAME-secgrp
+
+aws ec2 run-instances --image-id $UBUNTU_AMI --count 1 --instance-type t2.micro --key-name  $MYSSHKEYNAME --user-data cloud-init.sh --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$INSTNAME}]" --security-groups $INSTNAME-secgrp

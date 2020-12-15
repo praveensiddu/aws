394d4966cae7b61b4d788cb0f4462613bdd193e0
Praveen SIDDU <praveensiddu@users.noreply.github.com>
2020-12-15 10:49:21 -0500

    Create install-maven-ubuntu.sh

---
+++ utils/install-maven-ubuntu.sh
@@ -0,0 +1,8 @@
+#! /bin/bash
+
+
+wget https://mirrors.sonic.net/apache/maven/maven-3/3.6.3/binaries/apache-maven-3.6.3-bin.tar.gz -O apache-maven-3.6.3-bin.tar.gz 
+tar xzvf apache-maven-3.6.3-bin.tar.gz
+rm -f apache-maven-*-bin.tar.gz
+sudo mv apache-maven-3.6.3 /opt
+echo "export PATH=\$PATH:/opt/apache-maven-3.6.3/bin" >> ~/.bashrc

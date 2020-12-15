9551b1c768c0dbb26058279eb86c8dbbf5a90012
Praveen SIDDU <praveensiddu@users.noreply.github.com>
2020-12-15 11:00:19 -0500

    Update install-docker-ubuntu.sh

--- utils/install-docker-ubuntu.sh
+++ utils/install-docker-ubuntu.sh
@@ -8,5 +8,6 @@ sudo apt-key fingerprint 0EBFCD88
 sudo add-apt-repository    "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
 sudo apt-get update
 sudo apt-get install -y docker-ce docker-ce-cli containerd.io
+sudo apt install -y docker-compose
 sudo docker run hello-world
 

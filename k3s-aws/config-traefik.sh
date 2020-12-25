5e5cfd7af33638666db17a9cbedc40fc5a235a8a
Praveen SIDDU <praveensiddu@users.noreply.github.com>
2020-12-25 18:27:58 -0500

    Update config-traefik.sh

--- k3s-aws/config-traefik.sh
+++ k3s-aws/config-traefik.sh
@@ -10,7 +10,7 @@ openssl req -x509 -nodes -days 365 -newkey rsa:2048 -keyout ./$MYDOMAIN.key -out
 
 export MYDOMAIN_PUBLIC_CERT=$(base64 -w 0 ./$MYDOMAIN.cert)
 export MYDOMAIN_PRIV_KEY=$(base64 -w 0 ./$MYDOMAIN.key)
-wget https://raw.githubusercontent.com/praveensiddu/aws/main/k3s-aws/manifests/traefik_helm_values.yaml -O traefik_helm_values.yaml
+wget https://raw.githubusercontent.com/praveensiddu/aws/main/k3s-aws/manifests/traefik/traefik_helm_values.yaml -O traefik_helm_values.yaml
 sed -i "s/CHANGEME_MYDOMAIN_PRIV_KEY/$MYDOMAIN_PRIV_KEY/g" traefik_helm_values.yaml
 sed -i "s/CHANGEME_MYDOMAIN_PUBLIC_CERT/$MYDOMAIN_PUBLIC_CERT/g" traefik_helm_values.yaml
 sed -i "s/CHANGEME_MYDOMAIN/$MYDOMAIN/g" traefik_helm_values.yaml

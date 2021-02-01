d1215718693427ba618ea533a90c419f3ed6e57b
Praveen SIDDU <praveensiddu@users.noreply.github.com>
2021-02-01 00:17:52 -0500

    Update get-cert-letsencrypt.sh

--- bastion/get-cert-letsencrypt.sh
+++ bastion/get-cert-letsencrypt.sh
@@ -22,7 +22,8 @@ DOMAIN="$1" && sudo -E bash -c "cat /etc/letsencrypt/live/$DOMAIN/fullchain.pem 
 #Secure access to the combined file, which contains the private key, with this command:
 sudo chmod -R go-rwx /etc/haproxy/certs
 
-sudo [ ! -f /etc/haproxy/certs/yourdomain.pem ] && { echo "creating link to yourdomain.pem"; ln -s /etc/haproxy/certs/$1.pem /etc/haproxy/certs/yourdomain.pem; 
+sudo [ ! -f /etc/haproxy/certs/yourdomain.pem ] && { echo "creating link to yourdomain.pem"; ln -s /etc/haproxy/certs/$1.pem /etc/haproxy/certs/yourdomain.pem;  }
+
 
 
 cd /etc/haproxy

cd54f5778746f0a88cc6fbf9d0e0f6f1782211a0
Praveen SIDDU <praveensiddu@users.noreply.github.com>
2021-01-31 23:14:37 -0500

    Update create-ssh-key.sh

--- utils/create-ssh-key.sh
+++ utils/create-ssh-key.sh
@@ -6,4 +6,6 @@ fi
 
 # TBD RUn describe keys and check if the key is in aws.
 ssh-keygen -t rsa -b 4096 -C "bastion to internal server"  -f /home/ec2-user/.ssh/id_rsa -P ""
+aws ec2 delete-key-pair --key-name "bastion-to-other-hosts-key"
+
 aws ec2 import-key-pair --key-name "bastion-to-other-hosts-key" --public-key-material fileb://~/.ssh/id_rsa.pub
